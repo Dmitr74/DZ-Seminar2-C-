eed6cf607493ef79be75fbf01ca3198ba8a839a9
Dmitrii Lih <omts1dm@gmail.com>
2023-02-19 23:03:26 +0500

    задачи 10,13,15

---
+++ zadacha_13/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1000 && number > 99)
+{
+    int ThreeDigit = number % 10;
+    Console.WriteLine($"Третья цифра: {ThreeDigit}");
+}
+else if (number == 1000 || number > 1000)
+{
+    Console.Write("Некорректный ввод чисел.");
+}
+else if (number < 100)
+{
+    Console.Write("Третьей цифры нет.");
+}
